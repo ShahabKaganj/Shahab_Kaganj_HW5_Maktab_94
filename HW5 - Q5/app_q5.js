0b4bcca8854951081b32cd02f3fcb69a44ba7a86
Shahab Kaganj <shahab.kgnj@gmail.com>
2023-04-03 00:01:08 +0330

    Convert the declaration function to arrow function

--- HW5 - Q5/app_q5.js
+++ HW5 - Q5/app_q5.js
@@ -1,13 +1,17 @@
 
 // HW5 - Q5 ---------------
 
-function ask(question, yes, no) {
-    if (confirm(question)) yes();
-    else no();
-}
+// function ask(question, yes, no) {
+//     if (confirm(question)) yes();
+//     else no();
+// }
 
-ask(
-    "Do you agree?",
-    function () { alert("You agreed."); },
-    function () { alert("You canceled the execution."); }
-);
+// ask(
+//     "Do you agree?",
+//     function () { alert("You agreed."); },
+//     function () { alert("You canceled the execution."); }
+// );
+
+let ask = (question) => { (confirm(question)) ? alert("You agreed.") : alert("You canceled the execution.") };
+
+ask("Do You Agree?")
