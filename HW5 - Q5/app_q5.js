131c93468a074add9a2dfcfb0eab781e2d373436
Shahab Kaganj <shahab.kgnj@gmail.com>
2023-04-02 23:55:05 +0330

    Creating the initial form of the question

---
+++ HW5 - Q5/app_q5.js
@@ -0,0 +1,13 @@
+
+// HW5 - Q5 ---------------
+
+function ask(question, yes, no) {
+    if (confirm(question)) yes();
+    else no();
+}
+
+ask(
+    "Do you agree?",
+    function () { alert("You agreed."); },
+    function () { alert("You canceled the execution."); }
+);
