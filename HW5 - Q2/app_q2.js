defb6c11fd144f6ef276d4caba275c348f526dc0
Shahab Kaganj <shahab.kgnj@gmail.com>
2023-04-06 00:31:58 +0330

    add 2nd way resolve

--- HW5 - Q2/app_q2.js
+++ HW5 - Q2/app_q2.js
@@ -2,22 +2,38 @@
 
 let count = 0;
 
+// let user = {
+//   increase: function () {
+//     return count++;
+//   },
+
+//   decrease: function () {
+//     return count--;
+//   },
+
+//   reset: function () {
+//     count = 0;
+//   },
+
+//   read: function () {
+//     console.log(`${count}`);
+//   },
+// };
+
+// user.increase();
+// user.decrease();
+// user.decrease();
+// user.decrease();
+// user.reset();
+// user.read();
+
+//--------------------------------------------
+
 let user = {
-  increase: function () {
-    return count++;
-  },
-
-  decrease: function () {
-    return count--;
-  },
-
-  reset: function () {
-    count = 0;
-  },
-
-  read: function () {
-    console.log(`${count}`);
-  },
+    increase: () => count++,
+    decrease: () => count--,
+    reset: () => (count = 0),
+    read: () => console.log(`${count}`),
 };
 
 user.increase();
